d0d623cf24ca11041e317103eeb5827d4bfad734
ShermanNovak <sk33t0722@gmail.com>
2022-10-09 19:59:13 +0800

    wallet

--- screens/Wallet.js
+++ screens/Wallet.js
@@ -1,121 +1,136 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
-import React from 'react'
-import Body from '../components/Body.js'
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import React from "react";
+import Body from "../components/Body.js";
 
 const Wallet = () => {
   return (
     <ScrollView>
-    <Body title='My Wallet'>
-      <View style={styles.account}>
-        <Text style={styles.cronos}>My CRONOS Account</Text>
-        <View style={styles.value}>
+      <Body title="My Wallet">
+        <View style={styles.account}>
+          <Text style={styles.cronos}>My CRONOS Account</Text>
+          <View style={styles.value}>
             <Text style={styles.num}>12.789 CRO</Text>
+          </View>
         </View>
-      </View>
-      <Text class={styles.seeds}>100 Seeds = 1 CRO</Text>
+        <Text class={styles.seeds}>100 Seeds = 1 CRO</Text>
 
-      <View style={styles.titles}>
-        <Text style={{fontSize: 30, fontWeight: 'bold'}}>My Garden</Text>
-        <Image style={styles.onecolumn} source={require('../assets/random-succulent.png')}/>
-      </View>
-      <View>
-        <View>
-          <Text style={{fontSize: 30, fontWeight: 'bold', marginVertical: 20}}>Today's Growth</Text>
+        <View style={styles.titles}>
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>My Garden</Text>
+          <Image
+            style={styles.onecolumn}
+            source={require("../assets/random-succulent.png")}
+          />
         </View>
+        <View>
+          <View>
+            <Text
+              style={{ fontSize: 30, fontWeight: "bold", marginVertical: 20 }}
+            >
+              Today's Growth
+            </Text>
+          </View>
+          <View>
+            <View style={styles.plants}>
+              <View style={[styles.plant, { backgroundColor: "#A58772" }]}>
+                <Image source={require("../assets/tree-vector.png")} />
+              </View>
+              <View style={styles.plantdesc}>
+                <Text style={[styles.plant, { fontWeight: "bold" }]}>
+                  5 Seeds harvested
+                </Text>
+                <Text>Oct 09, 11:42</Text>
+              </View>
+            </View>
+          </View>
+
+          <View>
+            <View style={styles.plants}>
+              <View style={[styles.plant, { backgroundColor: "#A58772" }]}>
+                <Image source={require("../assets/tree-vector.png")} />
+              </View>
+              <View style={styles.plantdesc}>
+                <Text style={[styles.plant, { fontWeight: "bold" }]}>
+                  11 Seeds harvested
+                </Text>
+                <Text>Oct 09, 09:14</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View>
+          <Text
+            style={{ fontSize: 20, fontWeight: "bold", marginVertical: 20 }}
+          >
+            October 8
+          </Text>
+        </View>
+
         <View>
           <View style={styles.plants}>
-            <View style={[styles.plant, {backgroundColor: '#A58772'}]}>
-              <Image source={require('../assets/tree-vector.png')}/>
+            <View style={[styles.plant, { backgroundColor: "#cee8d7" }]}>
+              <Image source={require("../assets/desert-group-icon.png")} />
             </View>
             <View style={styles.plantdesc}>
-              <Text style={[styles.plant, {fontWeight: 'bold'}]}>5 Seeds harvested</Text>
+              <Text style={[styles.plant, { fontWeight: "bold" }]}>
+                100 CRO donated
+              </Text>
               <Text>Oct 09, 11:42</Text>
             </View>
           </View>
         </View>
-
-        <View>
-          <View style={styles.plants}>
-            <View style={[styles.plant, {backgroundColor: '#A58772'}]}>
-              <Image source={require('../assets/tree-vector.png')}/>
-            </View>
-            <View style={styles.plantdesc}>
-            <Text style={[styles.plant, {fontWeight: 'bold'}]}>11 Seeds harvested</Text>
-              <Text>Oct 09, 09:14</Text>
-            </View>
-          </View>
-        </View>
-      </View>
-      <View>
-        <Text style={{fontSize: 20, fontWeight: 'bold', marginVertical: 20}}>October 8</Text>
-      </View>
-
-      <View>
-        <View style={styles.plants}>
-          <View style={[styles.plant, {backgroundColor: '#cee8d7'}]}>
-            <Image source={require('../assets/desert-group-icon.png')}/>
-          </View>
-          <View style={styles.plantdesc}>
-          <Text style={[styles.plant, {fontWeight: 'bold'}]}>100 CRO donated</Text>
-            <Text>Oct 09, 11:42</Text>
-          </View>
-        </View>
-      </View>
-
-    </Body>
+      </Body>
     </ScrollView>
-  )
-}
+  );
+};
 
-export default Wallet
+export default Wallet;
 
 const styles = StyleSheet.create({
   account: {
-    backgroundColor: '#061222',
-    color: '#EBEBEB',
-    fontSize:14,
+    backgroundColor: "#061222",
+    color: "#EBEBEB",
+    fontSize: 14,
     borderRadius: 10,
     padding: 20,
-
   },
   num: {
     fontSize: 24,
-    color: '#EBEBEB'
+    color: "#EBEBEB",
   },
   seeds: {
     fontSize: 14,
-    color: '#535252',
+    color: "#535252",
     marginBottom: 10,
   },
-  cronos:{
-    marginBottom:15,
-    color: '#EBEBEB'
+  cronos: {
+    marginBottom: 15,
+    color: "#EBEBEB",
   },
   titles: {
-    marginTop:10,
+    marginTop: 10,
   },
   plant: {
     borderRadius: 20,
     width: 70,
     height: 70,
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center",
   },
   plantdesc: {
     paddingLeft: 20,
-    width: '80%'
+    width: "80%",
   },
   plant: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   value: {
-    display: 'flex',
-    flexDirection: 'row',
-    color: '#EBEBEB',
-  }, 
+    display: "flex",
+    flexDirection: "row",
+    color: "#EBEBEB",
+  },
   plants: {
-    display: 'flex',
-    flexDirection: 'row'
-  }
-})
+    display: "flex",
+    flexDirection: "row",
+  },
+});
